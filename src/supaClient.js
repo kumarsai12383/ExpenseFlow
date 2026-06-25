8e21e2791580ed34558e6d358bf4476ebec22d79
kumarsai12383 <kumarsai05072005@gmail.com>
2026-06-25 10:47:04 +0530

    added env

--- src/supaClient.js
+++ src/supaClient.js
@@ -1,5 +1,5 @@
-import {createClient} from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://azgxujswjcumgdborhes.supabase.co"
-const supabaseKey = "sb_publishable_tvCose1OhRrcTapxfqkPYA_9zNlr6U-"
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
